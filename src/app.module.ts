82f6ab421e42c15cba3c397c26f220301e6a7ab7
fares <hamoudaferes@gmail.com>
2021-01-29 12:34:21 +0100

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { StudentsModule } from './students/students.module';
 import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://nour:nour@cluster0.0ogrz.mongodb.net/pfe_manager?retryWrites=true&w=majority',{ dbName: 'test', useNewUrlParser: true }), UsersModule, StudentsModule, TeachersModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://nour:nour@cluster0.0ogrz.mongodb.net/pfe_manager?retryWrites=true&w=majority',{ dbName: 'test', useNewUrlParser: true }), UsersModule, StudentsModule, TeachersModule , PresentationModule , SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
